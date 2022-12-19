d4dbd73fccc3bc9b51d03afa7756a2ae6c083001
Payel <payel.nandy2580@gmail.com>
2022-12-19 22:36:44 +0530

    Inherit

---
+++ Car.java
@@ -0,0 +1,21 @@
+
+    class Vehicle{
+        protected String brand = "Ford";
+        public void honk(){
+            System.out.println("Tuut,Tuut !");
+        }
+    }
+    class Car extends Vehicle {
+        private String modelname = "Mustang";
+        public static void main(String[] args){
+            Car myCar= new Car();
+            myCar.honk();
+            System.out.println(myCar.brand+ " "+myCar.modelname);
+
+
+        }
+
+    }
+
+
+
