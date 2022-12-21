ad844e1819401c33a8a07a1048c9d72152c7a658
Payel <payel.nandy2580@gmail.com>
2022-12-22 00:41:21 +0530

    Java User Input

--- User.java
+++ User.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
- class User {
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class User {
      public static void main(String args[]) {
          Scanner myObj = new Scanner(System.in);
+         LocalTime myObj1 = LocalTime.now();
+         LocalDateTime myObj2 = LocalDateTime.now();
+         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
          System.out.println("Enter name, age and salary");
 
          String name = myObj.nextLine();
@@ -10,5 +17,14 @@ import java.util.Scanner;
          System.out.println("Name: "+ name);
          System.out.println("Age: "+ age);
          System.out.println("Salary: "+ salary);
+
+
+         System.out.println("Now the time is : "+ myObj1);
+         System.out.println("Today date and time is : "+ myObj2);
+         String formattedDate = myObj2.format(myFormatObj);
+         System.out.println("Formated date: "+ formattedDate);
+
      }
+
+
 }
